9ac683a3dd938f23f1603222722977a49728b6b3
nicolearmeltan <nico_15_tan@yahoo.com.ph>
2023-03-30 23:25:37 +0800

    first commit

---
+++ RoomBookingApp.Core.Tests/RoomBookingRequestProcessorTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Shouldly;
+using RoomBookingApp.Core.Models;
+using RoomBookingApp.Core.Processors;
+
+namespace RoomBookingApp.Core
+{
+    public class RoomBookingRequestProcessorTest
+    {
+        [Fact]
+        public void Should_Return_Room_Booking_Request_With_Request_Values()
+        {
+            //Arrange
+            var request = new RoomBookingRequest
+            {
+                FullName = "Test Name",
+                Email = "test@request.com",
+                Date = new DateTime(2023, 03, 30)
+            };
+
+            var processor = new RoomBookingRequestProcessor();
+
+            //Act
+            RoomBookingResult result = processor.BookRoom(request);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal(request.FullName, result.FullName);
+            Assert.Equal(request.Email, result.Email);
+            Assert.Equal(request.Date, result.Date);
+
+            result.ShouldNotBeNull();
+            result.FullName.ShouldBe(request.FullName);
+            result.Email.ShouldBe(request.Email);
+            result.Date.ShouldBe(request.Date);
+        }
+    }
+}
---
+++ RoomBookingApp.Core/Processors/RoomBookingRequestProcessor.cs
@@ -0,0 +1,21 @@
+﻿using RoomBookingApp.Core.Models;
+
+namespace RoomBookingApp.Core.Processors
+{
+    public class RoomBookingRequestProcessor
+    {
+        public RoomBookingRequestProcessor()
+        {
+        }
+
+        public RoomBookingResult BookRoom(RoomBookingRequest request)
+        {
+            return new RoomBookingResult
+            {
+                FullName = request.FullName,
+                Email = request.Email,
+                Date = request.Date,
+            };
+        }
+    }
+}
